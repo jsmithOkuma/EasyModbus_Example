33c8a53eacb67382a6deb2279945ed0ad0eb3d2d
Jacob Smith <jsmith@okuma.com>
2024-05-22 13:19:59 -0400

    Commit

---
+++ ModbusTCP_POC/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿
+namespace ModbusTCP_POC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblGetReg = new System.Windows.Forms.Label();
+            this.txtGetReg = new System.Windows.Forms.TextBox();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.btnSet = new System.Windows.Forms.Button();
+            this.txtRegVal = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtRegAdr = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbxRegType = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(305, 57);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 332);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(86, 13);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "Status - Stopped";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblGetReg);
+            this.groupBox1.Controls.Add(this.txtGetReg);
+            this.groupBox1.Controls.Add(this.btnGet);
+            this.groupBox1.Controls.Add(this.btnSet);
+            this.groupBox1.Controls.Add(this.txtRegVal);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtRegAdr);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cbxRegType);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 75);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(305, 254);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Change Data";
+            // 
+            // lblGetReg
+            // 
+            this.lblGetReg.AutoSize = true;
+            this.lblGetReg.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGetReg.Location = new System.Drawing.Point(133, 210);
+            this.lblGetReg.Name = "lblGetReg";
+            this.lblGetReg.Size = new System.Drawing.Size(0, 17);
+            this.lblGetReg.TabIndex = 9;
+            // 
+            // txtGetReg
+            // 
+            this.txtGetReg.Location = new System.Drawing.Point(23, 164);
+            this.txtGetReg.Name = "txtGetReg";
+            this.txtGetReg.Size = new System.Drawing.Size(84, 23);
+            this.txtGetReg.TabIndex = 8;
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(25, 203);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(82, 31);
+            this.btnGet.TabIndex = 7;
+            this.btnGet.Text = "Get";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // btnSet
+            // 
+            this.btnSet.Location = new System.Drawing.Point(151, 158);
+            this.btnSet.Name = "btnSet";
+            this.btnSet.Size = new System.Drawing.Size(121, 31);
+            this.btnSet.TabIndex = 6;
+            this.btnSet.Text = "Set";
+            this.btnSet.UseVisualStyleBackColor = true;
+            this.btnSet.Click += new System.EventHandler(this.btnSet_Click);
+            // 
+            // txtRegVal
+            // 
+            this.txtRegVal.Location = new System.Drawing.Point(151, 116);
+            this.txtRegVal.Name = "txtRegVal";
+            this.txtRegVal.Size = new System.Drawing.Size(121, 23);
+            this.txtRegVal.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(20, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "New Value";
+            // 
+            // txtRegAdr
+            // 
+            this.txtRegAdr.Location = new System.Drawing.Point(151, 71);
+            this.txtRegAdr.Name = "txtRegAdr";
+            this.txtRegAdr.Size = new System.Drawing.Size(121, 23);
+            this.txtRegAdr.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(20, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Register Address";
+            // 
+            // cbxRegType
+            // 
+            this.cbxRegType.FormattingEnabled = true;
+            this.cbxRegType.Items.AddRange(new object[] {
+            "Holding Register",
+            "Input Register",
+            "Digital Input",
+            "Coil Output"});
+            this.cbxRegType.Location = new System.Drawing.Point(151, 29);
+            this.cbxRegType.Name = "cbxRegType";
+            this.cbxRegType.Size = new System.Drawing.Size(121, 24);
+            this.cbxRegType.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(20, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Register Type";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(329, 354);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cbxRegType;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSet;
+        private System.Windows.Forms.TextBox txtRegVal;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtRegAdr;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.TextBox txtGetReg;
+        private System.Windows.Forms.Label lblGetReg;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ModbusTCP_POC/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EasyModbus;
+
+namespace ModbusTCP_POC
+{
+    public partial class Form1 : Form
+    {
+        ModbusServer ModServer;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (btnStart.Text == "START")
+            {
+                ModServer = new ModbusServer();
+                ModServer.Listen();
+                timer1.Enabled = true;
+                lblStatus.Text = "Status : Started";
+                btnStart.Text = "STOP";
+            } else if (btnStart.Text == "STOP")
+            {
+                ModServer.StopListening();
+                ModServer = null;
+                lblStatus.Text = "Status : Stopped";
+                 btnStart.Text = "START";
+            }
+        }
+
+        private void btnSet_Click(object sender, EventArgs e)
+        {
+            int iadr = int.Parse(txtRegAdr.Text);
+
+            if (cbxRegType.Text == "Holding Register")
+            {
+                try
+                {
+                    short ival = short.Parse(txtRegVal.Text);
+                    ModbusServer.HoldingRegisters regs = ModServer.holdingRegisters;
+                    regs[iadr] = ival;
+                } catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            } else if (cbxRegType.Text == "Input Register")
+            {
+                short ival = short.Parse(txtRegVal.Text);
+                ModbusServer.InputRegisters regs = ModServer.inputRegisters;
+                regs[iadr] = ival;
+            } else if (cbxRegType.Text == "Digital Input")
+            {
+                bool ival = bool.Parse(txtRegVal.Text);
+                ModbusServer.DiscreteInputs regs = ModServer.discreteInputs;
+                regs[iadr] = ival;
+            } else if (cbxRegType.Text == "Coil Output")
+            {
+                short ival = short.Parse(txtRegVal.Text);
+                ModbusServer.Coils regs = ModServer.coils;
+                regs[iadr] = true;
+            }
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+            //ModbusServer.HoldingRegisters regs = ModServer.holdingRegisters;
+            //lblGetReg.Text = regs[int.Parse(txtGetReg.Text)].ToString();
+
+            //timer1.Enabled = true;
+            ModbusServer.HoldingRegisters regs = ModServer.holdingRegisters;
+            int i = 0;
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            bool currentVal = ModServer.coils[30];
+            ModServer.coils[30] = !currentVal;
+        }
+    }
+}
